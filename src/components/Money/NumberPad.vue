58ca62130c342c6287199b7872bd65f8763e6ba7
Wang-XS <Wang-XS@hotmail.com>
2020-10-25 20:14:19 +0800

    基本完成 NumberPad 组件，还缺OK键

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -1,32 +1,70 @@
 <template>
     <div class="numberPad">
-        <div class="output">100</div>
+        <div class="output">{{output}}</div>
         <div class="buttons">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>删除</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>清空</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button class="ok">OK</button>
-            <button class="zero">0</button>
-            <button>.</button>
+            <button @click="inputContent">1</button>
+            <button @click="inputContent">2</button>
+            <button @click="inputContent">3</button>
+            <button @click="remove">删除</button>
+            <button @click="inputContent">4</button>
+            <button @click="inputContent">5</button>
+            <button @click="inputContent">6</button>
+            <button @click="clear">清空</button>
+            <button @click="inputContent">7</button>
+            <button @click="inputContent">8</button>
+            <button @click="inputContent">9</button>
+            <button >OK</button>
+            <button @click="inputContent" class="zero">0</button>
+            <button @click="inputContent">.</button>
         </div>
     </div>
 </template>
 <script lang="ts">
-    export default {
-      name:'NumberPad'
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+  import {MountOptions} from '@vue/test-utils';
+
+  @Component
+  export default class NumberPad extends Vue {
+    output = '0';
+
+    inputContent(event: MouseEvent) {
+      if (event.target) {
+        const button = event.target as HTMLButtonElement;
+        console.log(button.textContent);
+        const input = button.textContent!;
+        if (this.output === '0') {
+          if(input==='0'){return }
+          if ('123456789'.indexOf(input) >= 0) {
+            this.output = input
+          } else {
+            this.output += input
+          }
+          return;
+        }
+        if(this.output.indexOf('.')>=0){
+          if(input==='.'){return}
+        }
+        this.output += input
+      }
     }
+    remove(){
+      if(this.output.length===1){
+        this.output='0'
+      }else{
+        this.output=this.output.slice(0,-1)
+      }
+    }
+    clear(){
+      this.output='0'
+    }
+
+  }
 </script>
 
 <style lang="scss" scoped>
     @import "~@/assets/style/helper.scss";
+
     .numberPad {
         .output {
             @extend %clearFix;
@@ -35,10 +73,12 @@
             font-family: Consolas, monospace;
             padding: 9px 16px;
             text-align: right;
+            height: 72px;
         }
 
         .buttons {
             @extend %clearFix;
+
             > button {
                 width: 25%;
                 height: 64px;
@@ -64,18 +104,23 @@
                 &:nth-child(2), &:nth-child(5) {
                     background: darken($bg, 4%)
                 }
-                &:nth-child(3),&:nth-child(6),&:nth-child(9){
-                    background:darken($bg,4*2%)
+
+                &:nth-child(3), &:nth-child(6), &:nth-child(9) {
+                    background: darken($bg, 4*2%)
                 }
-                &:nth-child(4),&:nth-child(7),&:nth-child(10){
-                    background:darken($bg,4*3%)
+
+                &:nth-child(4), &:nth-child(7), &:nth-child(10) {
+                    background: darken($bg, 4*3%)
                 }
-                &:nth-child(8),&:nth-child(11),&:nth-child(13){
-                    background:darken($bg,4*4%)
+
+                &:nth-child(8), &:nth-child(11), &:nth-child(13) {
+                    background: darken($bg, 4*4%)
                 }
+
                 &:nth-child(14) {
                     background: darken($bg, 4*5%);
                 }
+
                 &:nth-child(12) {
                     background: darken($bg, 4*6%);
                 }
--- src/components/Money/Type.vue
+++ src/components/Money/Type.vue
@@ -10,7 +10,7 @@
 </template>
 <script lang="ts">
     import Vue from 'vue';
-    import {Component, Prop} from 'vue-property-decorator';
+    import {Component} from 'vue-property-decorator';
     @Component
     export default class Types extends Vue{
       type='-'
