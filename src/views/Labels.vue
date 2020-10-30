57efc523102b51d7273e36dfaae9b86c39e87d8f
Wang-XS <Wang-XS@hotmail.com>
2020-10-30 16:55:41 +0800

    完成 Labels HTML & CSS

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,6 +1,17 @@
 <template>
     <Layout>
-        <p>Labels</p>
+        <ol class="tags">
+            <li>
+                <span>衣</span>
+                <Icon name="right" ></Icon>
+            </li>
+            <li>食</li>
+            <li>住</li>
+            <li>行</li>
+        </ol>
+        <div class="createTag-wrapper">
+            <button class="createTag">新建标签</button>
+        </div>
     </Layout>
 </template>
 
@@ -11,5 +22,37 @@
 </script>
 
 <style scoped lang="scss">
+    .tags{
+        background:white;
+        font-size:16px;
+        padding-left: 16px;
+        >li{
+            min-height:44px;
+            display:flex;
+            align-items:center;
+            justify-content:space-between;
+            border-bottom:1px solid #e6e6e6
+        }
+        svg{
+            width:18px;
+            height:18px;
+            color:#666;
+            margin-right:16px;
+
+        }
+    }
+    .createTag{
+        background:#767676;
+        color:white;
+        border:none;
+        border-radius:4px;
+        height:40px;
+        padding:0 16px;
+        &-wrapper{
+            text-align:center;
+            padding:16px;
+            margin-top:44-16px;
+        }
+    }
 
 </style>
