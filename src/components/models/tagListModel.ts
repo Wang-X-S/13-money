d9751e867d1516a65949545418061f458a2b2fc6
Wang-XS <Wang-XS@hotmail.com>
2020-10-30 20:29:41 +0800

    展示和创建标签

--- src/components/models/tagListModel.ts
+++ src/components/models/tagListModel.ts
@@ -1,15 +1,20 @@
 const localStorageKeyName = 'tagList';
+type Tag={
+  id: string;
+  name: string;
+}
 type TagListModel = {
-  data: string[];
-  fetch: () => string[] ; //输入类型 => 输出类型
+  data: Tag[];
+  fetch: () => Tag[] ; //输入类型 => 输出类型
   create: (name: string) => 'success'|'duplicated';//联合类型
   save: () => void;
 }
 const tagListModel: TagListModel = {  //:TagListModel 关联起来
   data: [],
   create(name) {
-    if(this.data.indexOf(name)>=0){return 'duplicated'}
-    this.data.push(name);
+    const names = this.data.map(item=>item.name)//把data 里面所有的 name 搜索出来，产生一个新数组
+    if(names.indexOf(name)>=0){return 'duplicated'}
+    this.data.push({id:name,name:name});
     this.save();
     return 'success'
   },
