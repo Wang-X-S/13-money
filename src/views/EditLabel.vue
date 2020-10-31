e5da08a3b6a238e00980a727369ebdedb0affa38
Wang-XS <Wang-XS@hotmail.com>
2020-10-31 15:28:09 +0800

    改造 Notes.vue 使其成为通用组件

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,6 +1,12 @@
 <template>
     <Layout>
-        编辑标签
+        <div>
+            <icon name="left"></icon>
+            <span>
+                编辑标签
+            </span>
+        </div>
+        <Notes field-name="备注" placeholder="请输入备注"/>
     </Layout>
 </template>
 
@@ -8,8 +14,10 @@
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
   import tagListModel from '@/components/models/tagListModel';
-
-  @Component
+  import Notes from '@/components/Money/Notes.vue';
+  @Component({
+    components: {Notes}
+  })
   export default class EditLabel extends Vue {
     created() {
       const id = this.$route.params.id;
@@ -18,8 +26,8 @@
       const tag = tags.filter(t => t.id === id)[0];//filter 一定会返回一个数组,对于它每一个tag的id就等于当前的id
       if (tag) {
         console.log(tag);
-      }else{
-        this.$router.replace('/404') //重定向 404页面
+      } else {
+        this.$router.replace('/404'); //重定向 404页面
       }
     }
   }
