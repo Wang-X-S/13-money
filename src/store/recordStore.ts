48abc58b29aba2fba486683776b969d248f348cb
Wang-XS <Wang-XS@hotmail.com>
2020-11-03 16:39:06 +0800

    模块化的 store

---
+++ src/store/recordStore.ts
@@ -0,0 +1,7 @@
+import recordListModel from '@/components/models/recordListModel';
+
+export default {
+  //record store
+  recordList: recordListModel.fetch(),
+  createRecord: (record: RecordItem) => recordListModel.create(record),
+}
---
+++ src/store/tagStore.ts
@@ -0,0 +1,23 @@
+import tagListModel from '@/components/models/tagListModel';
+
+export default {
+  //tag store
+  tagList: tagListModel.fetch(),
+  createTag: (name: string) => {
+    const message = tagListModel.create(name);
+    if (message === 'duplicated') {
+      window.alert('标签名重复');
+    } else if (message === 'success') {
+      window.alert('添加成功');
+    }
+  },
+  findTag(id: string) {
+    return this.tagList.filter(t => t.id === id)[0];
+  },
+  removeTag: (id: string) => {
+    return tagListModel.remove(id);
+  },
+  updateTag: (id: string, name: string) => {
+    return tagListModel.update(id, name);
+  },
+}
