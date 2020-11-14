62faf71ae25c51ecc486b2a36615af6acffda6be
Wang-XS <Wang-XS@hotmail.com>
2020-11-14 19:38:14 +0800

    完成列表 CSS

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -3,17 +3,16 @@
         <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
         <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
         <div>
-            type: {{type}}
-            <br/>
-            interval: {{interval}}
-        </div>
-        <div>
             <ol>
                 <li v-for="(group,index) in result" :key="index">
-                    <h3>{{group.title}}</h3>
+                    <h3 class="title">{{group.title}}</h3>
                     <ol>
-                        <li v-for="item in group.items" :key="item.id">
-                            {{item.amount}} {{item.createdAt}}
+                        <li v-for="item in group.items" :key="item.id"
+                            class="record"
+                        >
+                           <span>{{tagString(item.tags)}}</span>
+                            <span class="notes">{{item.notes}}</span>
+                            <span>￥{{item.amount}}</span>
                         </li>
                     </ol>
                 </li>
@@ -34,6 +33,10 @@
     components: {Tabs}
   })
   export default class Statistics extends Vue {
+    tagString(tags: Tag[]){
+      return tags.length===0?'无' : tags.join(',')
+    }
+
     get recordList(){
       return  (this.$store.state as RootState).recordList
     }
@@ -78,4 +81,24 @@
         height:40px;
         font-size: 20px;
     }
+    %item{
+        padding: 8px 16px;
+        line-height:24px;
+        display:flex;
+        justify-content:space-between;
+        align-content:center;
+    }
+    .title{
+        @extend %item
+    }
+    .record{
+        background: white;
+        @extend %item
+    }
+    .notes{
+        margin-right:auto;
+        margin-left:16px;
+        color:#999;
+    }
+
 </style>
