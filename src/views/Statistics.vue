52e47ec6949fd0000ebbc8b512fcd688fb7caef6
Wang-XS <Wang-XS@hotmail.com>
2020-11-15 21:01:41 +0800

    数据排序分组日期

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -4,7 +4,7 @@
         <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
         <div>
             <ol>
-                <li v-for="(group,index) in result" :key="index">
+                <li v-for="(group,index) in groupedList" :key="index">
                     <h3 class="title">{{beautify(group.title)}}</h3>
                     <ol>
                         <li v-for="item in group.items" :key="item.id"
@@ -29,6 +29,7 @@
   import intervalList from '@/constants/intervalList';
   import recordTypeList from '@/constants/recordTypeList';
   import dayjs from 'dayjs';
+  import clone from '@/lib/clone';
 
   @Component({
     components: {Tabs}
@@ -57,18 +58,23 @@
     get recordList(){
       return  (this.$store.state as RootState).recordList
     }
-    get result(){
+    get groupedList(){
       const {recordList} = this;
-      type HashTableValue = {title: string; items: RecordItem[]};
-      const hashTable: {[key: string]: HashTableValue}= {};
-      for(let i= 0;i<recordList.length;i++){
-        const [date,time] = recordList[i].createdAt!.split('T');
-        hashTable[date] = hashTable[date] || {title: date,items: []}
-        hashTable[date].items.push(recordList[i])
-
+      if(recordList.length===0){return []}
+      //type HashTableValue = {title: string; items: RecordItem[]};
+      //const hashTable: {title: string;items: RecordItem[]}[];
+      const newList = clone(recordList).sort((a,b)=>dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf())
+      const result = [{title: dayjs(newList[0].createdAt).format('YYYY-MM-DD'),items:[newList[0]]}]
+      for(let i = 1;i < newList.length;i++){
+        const current = newList[i];
+        const last = result[result.length-1];
+        if(dayjs(last.title).isSame(dayjs(current.createdAt),'day')){
+          last.items.push(current)
+        }else{
+          result.push({title: dayjs(current.createdAt).format('YYYY-MM-DD'),items: [current]})
+        }
       }
-      console.log(hashTable)
-      return hashTable
+      return result
     }
     beforeCreate(){
       this.$store.commit('fetchRecords')
