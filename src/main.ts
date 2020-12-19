17257618714732466636b6ac15ad61fd7ba5e00b
Wang-XS <Wang-XS@hotmail.com>
2020-12-19 18:56:43 +0800

    保证用户用手机打开页面，否则提示用户

--- src/main.ts
+++ src/main.ts
@@ -18,3 +18,21 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app');
+
+if(document.documentElement.clientWidth > 500){
+  window.alert('请用手机打开本页面，以保证浏览效果')
+  //获取标签
+  const img = document.createElement('img')
+
+  img.src = 'qrcode.png'
+  //居中
+  img.style.position = 'fixed'
+  img.style.left = '50%'
+  img.style.top = '50%'
+  img.style.transform = 'translate(-50%,-50%)'
+
+  img.style.boxShadow = '0 0 10px rgba(0,0,0,25)'
+  //放到页面
+  document.body.appendChild(img)
+
+}
